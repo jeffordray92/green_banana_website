62b1a53c90c56c27389c8f9abf50a05b9ae272a8
Rex Louie Pilongo <rex@ingenuity-tech.com>
2015-09-23 09:57:12 +0800

    Initial Booking page

--- static/js/main.js
+++ static/js/main.js
@@ -22,6 +22,26 @@ function misc(){
      });
 }
 
+function bookForm(){
+
+    function floatLabel(inputType){
+        $(inputType).each(function(){
+            var $this = $(this);
+            // on focus add cladd active to label
+            $this.focus(function(){
+                $this.next().addClass("active");
+            });
+            //on blur check field and remove class if needed
+            $this.blur(function(){
+                if($this.val() === '' || $this.val() === 'blank'){
+                    $this.next().removeClass();
+                }
+            });
+        });
+    }
+    // just add a class of "floatLabel to the input field!"
+    floatLabel(".floatLabel");
+}
 function tabs(){
     // Variables
     var clickedTab = $(".tabs > .active");
@@ -165,7 +185,7 @@ $(document).ready(function() {
     misc();
     scrollSmooth(); 
     menuIcon();
-
+    bookForm();
     tabs();
 });
 
